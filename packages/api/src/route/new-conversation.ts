fe7f941dfc537ae73273e80c14f6138c79aee12e
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-05 19:35:07 +0330

    lint: import type

--- packages/api/src/route/new-conversation.ts
+++ packages/api/src/route/new-conversation.ts
@@ -1,11 +1,12 @@
 import {Region, StoreFileExtension, StoreFileType} from '@alwatr/store-engine';
-import {ChatCompletionMessageParam} from 'openai/resources/index.mjs';
+
 
 import {chat} from '../lib/openai-api';
 import {nanoServer} from '../lib/server';
 import {store} from '../lib/store';
 
 import type {AlwatrServiceResponse, StringifyableRecord} from '@alwatr/type';
+import type {ChatCompletionMessageParam} from 'openai/resources/index.mjs';
 
 /**
  * PUT `/conversation` create a new conversation.
