4cdf20a593a06f428ca259dd8dbccbed8ac31050
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-05 21:58:23 +0330

    feat(bot): on message

--- packages/telegram-bot/src/bot/on-message.ts
+++ packages/telegram-bot/src/bot/on-message.ts
@@ -1,5 +1,32 @@
-import {bot} from '../lib/bot.js';
+import {serviceRequest} from '@alwatr/fetch';
+
+import {bot} from '../lib/bot';
+import {config, logger} from '../lib/config';
+import {userCollection} from '../lib/user.js';
+
+import type {AlwatrServiceResponse} from '@alwatr/type';
 
 bot.on('message', async (ctx) => {
-  await ctx.reply('Hello!');
+  logger.logMethodArgs?.('on-message', {userId: ctx.from.id});
+
+  const conversationId = userCollection.get(ctx.from.id).conversationId;
+
+  const message = await ctx.reply('🧠 Processing...');
+  const response = await serviceRequest<AlwatrServiceResponse<{content: string; role: string}>>({
+    url: config.chatApi.baseUrl + '/chat',
+    method: 'PATCH',
+    timeout: 60_000,
+    retry: 0,
+    bodyJson: {
+      message: ctx.message.text,
+    },
+    queryParameters: {userId: ctx.from.id.toString(), conversationId},
+  });
+
+  if (!response.ok) {
+    logger.error('command-start', 'create_conversation_failed');
+    return;
+  }
+
+  await ctx.api.editMessageText(ctx.chat.id, message.message_id, response.data.content);
 });
--- packages/telegram-bot/src/bot/start-command.ts
+++ packages/telegram-bot/src/bot/start-command.ts
@@ -1,6 +1,47 @@
-import {bot} from '../lib/bot';
-import {config} from '../lib/config';
+import {serviceRequest} from '@alwatr/fetch';
 
+import {bot} from '../lib/bot';
+import {config, logger} from '../lib/config';
+import {userCollection} from '../lib/user.js';
+
+import type {AlwatrServiceResponse} from '@alwatr/type';
+
+/**
+ * Command `/start` is used to initialize the user account.
+ *
+ * 1. If the user account does not exist, create a new account.
+ *   1.1. Create a new conversation in the chat API.
+ *   1.2. Create a new user account in the store with the conversation ID.
+ * 2. If the user account exists, welcome the user.
+ */
 bot.command('start', async (ctx) => {
-  await ctx.reply(config.botMessage.start);
+  logger.logMethodArgs?.('start', {userId: ctx.from?.id});
+  if (ctx.from == null) return;
+
+  if (!userCollection.exists(ctx.from.id)) {
+    const message = await ctx.reply('Initializing Yo Mind Swap account 🧠');
+    const response = await serviceRequest<AlwatrServiceResponse<{conversationId: string}>>({
+      url: config.chatApi.baseUrl + '/conversation',
+      method: 'PUT',
+      timeout: 60_000,
+      retry: 0,
+      queryParameters: {
+        userId: ctx.from.id.toString(),
+      },
+    });
+
+    if (!response.ok) {
+      logger.error('command-start', 'create_conversation_failed');
+      return;
+    }
+
+    userCollection.create(ctx.from.id.toString(), {
+      conversationId: response.data.conversationId,
+    });
+
+    await ctx.api.editMessageText(ctx.chat.id, message.message_id, 'Your Mind Swap account created 🧠🎉');
+  }
+  else {
+    await ctx.reply('Welcome to Mind Swap 🧠');
+  }
 });
