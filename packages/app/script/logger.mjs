b49e5bab825a8243cef591a47fbd150b6a8ac679
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-02-07 21:51:16 +0330

    chore: remove app

--- packages/app/script/logger.mjs
+++
@@ -1,7 +0,0 @@
-import {createLogger, definePackage} from '@alwatr/logger';
-definePackage('mind-swap-app/11ty', '0.x');
-
-export const logger = createLogger('mind-swap-app', true);
-
-export const devMode = process.env.NODE_ENV !== 'production';
-logger.logProperty?.('devMode', devMode);
